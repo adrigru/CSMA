c8663c3829812f6dcba1ef6ac2f7d6808f5cda5b
adeaeede <h4rrytrum4n@mi.fu-berlin.de>
2021-01-30 10:58:37 +0100

    Implement exporting of comments.

--- README.md
+++ README.md
@@ -5,8 +5,7 @@ Project repository for the "Critical Social Media Analysis" seminar on FU Berlin
 ## Set up prerequisites and credentials
 Follow the Google's Python Quickstart Guide [here](https://developers.google.com/youtube/v3/quickstart/python)
 
-- Create and download your client_secret.json and paste it under `youtube_api/config/secrets.json`
-- Make sure the json file containing the secrets is names `secrets.json`
+- Create an API_KEY within the Google developer console https://console.developers.google.com/
 - Create a python virtual environment by running `python3 -m venv venv`
 - Make sure you use the virtual environment by running `source venv/bin/activate`
 - Install the necessary dependencies `pip3 install -r requirements.txt`
@@ -18,10 +17,17 @@ Follow the Google's Python Quickstart Guide [here](https://developers.google.com
 ```
 usage: search.py [-h]
                  [-sort_by {date,rating,relevance,title,videoCount,viewCounts}]
-                 [-query QUERY] [-max_results MAX_RESULTS]
-                 [-related_to RELATED_TO] [--export]
+                 [-max_results MAX_RESULTS] [-related_to RELATED_TO]
+                 [--export]
+                 api_key query
 
 Find YouTube videos.
+
+positional arguments:
+  api_key               YouTube API is required. Check:
+                        https://console.developers.google.com/
+  query                 The q parameter specifies the query term to search
+                        for.
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -29,8 +35,6 @@ optional arguments:
                         The order parameter specifies the method that will be
                         used to order resources in the API response. The
                         default value is relevance.
-  -query QUERY          The q parameter specifies the query term to search
-                        for.
   -max_results MAX_RESULTS
                         The maxResults parameter specifies the maximum number
                         of items that should be returned in the result set.
@@ -46,6 +50,14 @@ optional arguments:
 
 ## Example usage
 
-- Run the script with `python3 -query 'global warming'` to search for global warming related videos
-- Click on the url provided by Google and authorize the application
-- Copy the code from the browser and past it into the application
+- Run the script with `python3 API_KEY "chemtrails"` to search for global warming related videos
+- You can export search results in to a `.csv` file with `python3 API_KEY "chemtrails" --export` 
+- Look into the csv file and find a relevant video's id
+- Put the videoId as an argument for `-related_to` flag, for instance `python3 API_KEY "chemtrails" -related_to=uOMRF7t5Vn0 --export`
+- The script will search for 25 related videos and store the comments in a `.csv` file
+- Be patient it might take a couple of minutes depending on number of results  
+
+## Remarks
+Sometimes when using the related_to parameter YouTube API returns irrelevant videos.
+Please make sure (by reading the video titles) if everything is fine.
+You can remove the comments for unrelated videoId's from the ``.csv`` file or manually select which videoIds you want to download and use `get_comments_by_vid_id` method.
--- youtube_api/search.py
+++ youtube_api/search.py
@@ -5,63 +5,146 @@
 # https://developers.google.com/explorer-help/guides/code_samples#python
 
 import argparse
+import csv
 import json
 import os
 import pprint
 from datetime import datetime as dt
 from io import StringIO
 
-import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 import pandas as pd
 
-scopes = ['https://www.googleapis.com/auth/youtube.force-ssl']
+
+def export(_dict, query):
+    filename = f'comments_{"_".join(query.split())}_{dt.now().isoformat()}.csv'
+    header = ['videoId', 'videoPublishedAt', 'videoTitle', 'commentId', 'replyCount', 'textOriginal',
+              'authorDisplayName', 'likeCount', 'publishedAt', 'parentId']
+    with open(os.path.join(f'{os.curdir}/results', filename), 'w+', newline='') as fp:
+        writer = csv.writer(fp, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(header)
+        for item in _dict:
+            video_data = [item['videoId'], item['publishTime'], item['title']]
+            if 'comments' in item:
+                for c in item['comments']:
+                    writer.writerow(
+                        video_data + [c['commentId'], c['replyCount'], c['textOriginal'], c['authorDisplayName'],
+                                      c['likeCount'], c['publishedAt'], None])
+                    if 'replies' in c:
+                        for r in c['replies']:
+                            writer.writerow(
+                                video_data + [r['replyId'], 0, r['textOriginal'],
+                                              r['authorDisplayName'],
+                                              r['likeCount'], r['publishedAt']])
 
 
-def search(query, sort_by, max_results, related_to, _type='video'):
-    # Disable OAuthlib's HTTPS verification when running locally.
-    # *DO NOT* leave this option enabled in production.
-    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+class YouTubeAPI:
+    def __init__(self, api_key):
+        # Disable OAuthlib's HTTPS verification when running locally.
+        # *DO NOT* leave this option enabled in production.
+        os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+        scopes = ['https://www.googleapis.com/auth/youtube.force-ssl']
 
-    api_service_name = 'youtube'
-    api_version = 'v3'
-    client_secrets_file = os.path.join(os.path.abspath(os.curdir), 'config/secrets.json')
+        # Get credentials and create an API client
+        api_service_name = 'youtube'
+        api_version = 'v3'
+        self.youtube = googleapiclient.discovery.build(
+            api_service_name, api_version, developerKey=api_key)
 
-    # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+    def get_comments_for_vids(self, query, sort_by, max_results, related_to='', _export=False):
+        videos = self.search(query, sort_by, max_results, related_to=related_to)
 
-    if related_to is not '':
-        request = youtube.search().list(
-            relatedToVideoId=related_to,
-            maxResults=max_results,
-            order=sort_by,
+        for video in videos:
+            comments = self.get_comments_by_vid_id(video['videoId'], sort_by, replies=True)
+            if comments is not None:
+                video['comments'] = comments
+
+        if _export:
+            return export(videos, query)
+
+        return videos
+
+    def get_comments_by_vid_id(self, video_id, sort_by, max_results=100, replies=False, _export=False):
+        request = self.youtube.commentThreads().list(
             part='snippet',
-            type=_type,
-            q=query
-        )
-    else:
-        request = youtube.search().list(
             maxResults=max_results,
+            moderationStatus='published',
             order=sort_by,
-            part='snippet',
-            type=_type,
-            q=query
+            videoId=video_id
         )
+        try:
+            response = request.execute()
+            results = [{'commentId': c['id'],
+                        'videoId': c['snippet']['videoId'],
+                        'replyCount': c['snippet']['totalReplyCount'],
+                        'textOriginal': c['snippet']['topLevelComment']['snippet']['textOriginal'],
+                        'authorDisplayName': c['snippet']['topLevelComment']['snippet']['authorDisplayName'],
+                        'likeCount': c['snippet']['topLevelComment']['snippet']['likeCount'],
+                        'publishedAt': c['snippet']['topLevelComment']['snippet']['publishedAt']} for c in
+                       response['items']]
 
-    response = request.execute()
-    _json = json.dumps([{'videoId': v['id']['videoId'],
-                         'title': v['snippet']['title'],
-                         'channelTitle': v['snippet']['channelTitle'],
-                         'description': v['snippet']['description'],
-                         'publishTime': v['snippet']['publishTime']} for v in response['items']])
+            if replies:
+                for comment in results:
+                    if comment['replyCount']:
+                        replies = self.get_replies(max_results=100, parent_id=comment['commentId'])
+                        comment['replies'] = replies
 
-    pprint.pprint(_json)
-    return _json
+            if _export:
+                return json.dumps(results)
+
+            return results
+
+        except Exception as e:
+            print(f'An exception has occurred: {e}')
+
+        return None
+
+    def get_replies(self, max_results, parent_id):
+        request = self.youtube.comments().list(
+            part="snippet",
+            maxResults=max_results,
+            parentId=parent_id
+        )
+        response = request.execute()
+        results = [{'replyId': r['id'],
+                    'parentId': r['snippet']['parentId'],
+                    'authorDisplayName': r['snippet']['authorDisplayName'],
+                    'textOriginal': r['snippet']['textOriginal'],
+                    'likeCount': r['snippet']['likeCount'],
+                    'publishedAt': r['snippet']['publishedAt']} for r in response['items']]
+
+        return results
+
+    def search(self, query, sort_by, max_results, related_to='', _type='video', _export=False):
+        if related_to is not '':
+            request = self.youtube.search().list(
+                relatedToVideoId=related_to,
+                maxResults=max_results,
+                order=sort_by,
+                part='snippet',
+                type=_type,
+                q=query
+            )
+        else:
+            request = self.youtube.search().list(
+                maxResults=max_results,
+                order=sort_by,
+                part='snippet',
+                type=_type,
+                q=query
+            )
+
+        response = request.execute()
+        results = [{'videoId': v['id']['videoId'],
+                    'title': v['snippet']['title'],
+                    'channelTitle': v['snippet']['channelTitle'],
+                    # 'description': v['snippet']['description'],
+                    'publishTime': v['snippet']['publishTime']} for v in response['items']]
+        if _export:
+            return json.dumps(results)
+
+        return results
 
 
 if __name__ == '__main__':
@@ -71,7 +154,11 @@ if __name__ == '__main__':
                         default='relevance',
                         help='The order parameter specifies the method that will be used to order resources'
                              ' in the API response. The default value is relevance.')
-    parser.add_argument('-query',
+    parser.add_argument('api_key',
+                        type=str,
+                        default='',
+                        help='YouTube API is required. Check: https://console.developers.google.com/')
+    parser.add_argument('query',
                         type=str,
                         default='',
                         help='The q parameter specifies the query term to search for.')
@@ -91,9 +178,25 @@ if __name__ == '__main__':
                         help='The export parameter specifies if the results should be exported as .csv file')
     args = parser.parse_args()
 
-    _json = search(args.query, args.sort_by, args.max_results, args.related_to)
+    ytb = YouTubeAPI(args.api_key)
 
+    # Search for videos related to query
+    _json = ytb.search(args.query, args.sort_by, args.max_results, related_to=args.related_to, _export=args.export)
+    pprint.pprint(_json)
+
+    # Export list of videos that YouTube returned for the given query
     if args.export:
         df = pd.read_json(StringIO(_json), orient='records')
-        filename = f'results_{"_".join(args.query.split())}_{args.sort_by}_{dt.now().isoformat()}.csv'
+        filename = f'videos_{"_".join(args.query.split())}_{args.sort_by}_{dt.now().isoformat()}.csv'
         df.to_csv(path_or_buf=os.path.join(f'{os.curdir}/results', filename), index=False)
+
+    # Export comments to videos related to videoId specified in related_to argument
+    if args.related_to:
+        _json = ytb.get_comments_for_vids(args.query, args.sort_by, args.max_results, related_to=args.related_to,
+                                          _export=args.export)
+    # Export comments for videos related to query
+    # Might return irrelevant comments
+    # Better specify related_to or make sure the videos found by YouTube API is what you want
+    else:
+        _json = ytb.get_comments_for_vids(args.query, args.sort_by, args.max_results, related_to='',
+                                          _export=args.export)
